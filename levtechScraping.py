ecc91bcf89fbedd2ab7e97ed892498325ab83ae6
shoma <robbykuniscute@gmail.com>
2021-08-10 10:01:35 +0900

    レビュー反映

--- levtechScraping.py
+++ levtechScraping.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 # 区切り文字
 DELIMITER = '###'
 # MAXプロセス数
-PROC_IDX = 15
+MAX_PROC = 15
 # レバテックURL
 levtech_url = 'https://freelance.levtech.jp/project/search/?keyword=&district[]=1&area[]=1&area[]=2&area[]=3&area[]=4&area[]=5&area[]=6&area[]=7&area[]=8&area[]=9&area[]=10&area[]=11&area[]=12&area[]=13&area[]=14&area[]=15&area[]=16&sala=7'
 
@@ -38,22 +38,21 @@ def get_project_detail(link):
         project_tsv += DELIMITER
 
         # プログラミング言語
-        lang_csv = ''
+        lang_tsv = ''
         for lang in soup.find_all('a', {'class': 'pjtTag'}):
             if str(lang.get('href')).startswith('/project/skill'):
                 # 余分なタブを入れない
-                if len(lang_csv) > 0 : lang_csv += '\t'
-                lang_csv += lang.text
+                if len(lang_tsv) > 0 : lang_tsv += '\t'
+                lang_tsv += lang.text
         
         # 改行コードをフィックス
-        project_tsv += lang_csv + '\n' 
+        project_tsv += lang_tsv + '\n' 
 
     return project_tsv
 
 #--------------------------------
 # メインの処理
 # 「Next」リンクが有効なら続行
-
 while len(levtech_url) > 0 :
 
     # レバテックの案件一覧(23区で絞り込み)URL
@@ -62,10 +61,6 @@ while len(levtech_url) > 0 :
     # レスポンスの HTML から BeautifulSoup オブジェクトを作る
     soup = BeautifulSoup(res.text, 'html.parser')
 
-    # 「Next」リンクが有効なら格納
-    next_page = soup.find('a', {'rel': 'next'}).get('href')
-    levtech_url = '' if next_page is None else 'https://freelance.levtech.jp' + next_page
-
     # ページに含まれるリンクを全て取得する
     links = [url.get('href') for url in soup.find_all('a', {'class': 'js-link_rel'})]
 
@@ -73,7 +68,7 @@ while len(levtech_url) > 0 :
     project_tsv = ''
     
     # 案件詳細から情報取得(マルチプロセス)
-    with concurrent.futures.ProcessPoolExecutor(max_workers=PROC_IDX) as executor:
+    with concurrent.futures.ProcessPoolExecutor(max_workers=MAX_PROC) as executor:
         futures = [executor.submit(get_project_detail,link) for link in links]
 
         # 詳細情報が取得できた順に格納
@@ -83,3 +78,7 @@ while len(levtech_url) > 0 :
 
     print(project_tsv)
     # TODO：GoにRestApiする
+
+    # 「Next」リンクが有効なら格納
+    next_page = soup.find('a', {'rel': 'next'}).get('href')
+    levtech_url = '' if next_page is None else 'https://freelance.levtech.jp' + next_page
